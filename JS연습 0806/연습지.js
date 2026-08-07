d7473272ffe95650ac602cc352c09653dfa18ec3
salamance95 <a01086189349@gmail.com>
2026-08-07 11:39:49 +0900

    js

--- JS연습 0806/연습지.js
+++ JS연습 0806/연습지.js
@@ -1,25 +1,21 @@
-let age = -1;
-if (age >= 20) {
-  console.log("성인");
-} else if (age >= 17) {
-  console.log("고등학생");
-} else if (age >= 14) {
-  console.log("중학생");
-} else if (age >= 8) {
-  console.log("초등학생");
-} else if (age >= 0) {
-  console.log("유아");
-} else {
-  console.log("정자");
+//while 문과 for문을 사용하는 경우
+//for 문: 반복되는 횟수를 미리 알 때, 초기화;조건;증감 이 한줄에 모여있음
+//while 문: 조건이 만족될때까지 사용, 흩어져있어 빠뜨리기 쉬움
+//조건이 참인 동안 반복 - 증감식이 포함되어있지 않아 직접 챙겨야 함
+let i = 1;
+let j = 1;
+while (i < 10) {
+  while (j < 10) {
+    console.log(`${i}x${j}= ${i * j}`);
+    j++;
+  }
+  i++;
+  j = 1; // j 값 초기화
 }
-
-//if 중첩
-let isSubmitted = true;
-let reportScore = 65;
-if (isSubmitted) {
-  if (reportScore < 80) {
-    console.log("보충 과제 대상");
-  } else {
-    console.log("통과");
+let sum = 0;
+for (let n = 0; n <= 100; n++) {
+  if (n % 2 == 0 || n % 5 == 0) {
+    sum += n;
   }
 }
+console.log(sum);
